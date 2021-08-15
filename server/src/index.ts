9d92ca026eb6cdbabd12ab1068a0e2808aec963f
BBennettChiba <BrysonCBennett@gmail.com>
2021-08-15 16:02:17 +0900

    changed dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,12 @@
 import express from "express";
 import path from "path";
 import fs from "fs";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(express.static(path.join(__dirname, "../../build")));
 
 app.get("/", (_, res) => {
