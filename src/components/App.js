ab44b5ae2c092178e44bb1b3d289b44f9300ecd1
BBennettChiba <BrysonCBennett@gmail.com>
2021-08-15 19:03:25 +0900

    changed endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000";
 
 export default function App() {
   const [input, setInput] = useState("");
